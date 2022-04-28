03e3e194d488b2761e06d1a81b7d9ba3be7af28c
mperea <martin.perea.hernandez@gmail.com>
2022-04-28 14:59:37 -0500

    modificacion al archivo pokemon.test

--- weekly_mission_2/unit_testing/pokemon.test.js
+++ weekly_mission_2/unit_testing/pokemon.test.js
@@ -1,6 +1,6 @@
 import Pokemon from './pokemon'
 
-test ('1 Create a new object pokemon', () => {
+test ('Test 1) Create a new object pokemon', () => {
   const myPokemon = new Pokemon('Charmander')
   expect(myPokemon.name).toBe('Charmander'); // Corrige esta prueba
 });
