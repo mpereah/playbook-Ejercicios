06cefd0145760ec7279552491a3c564aea1936ad
mpereah <martin.perea.hernandez@gmail.com>
2022-05-03 17:10:18 -0500

    agregando parametros para express, se ejecuta con node app.js y se para con ctrl + c, se abre en localhost:3000

--- weekly_mission_3/express_basic/app.js
+++ weekly_mission_3/express_basic/app.js
@@ -9,7 +9,20 @@ const port = 3000
 
 // path imicial, repondera a localhost
 app.get('/', (req, res) => {
-    res.send('Hello world')
+    res.send('Hello world.................')
+})
+
+app.get('/launchx', (req, res) => {
+    res.send('Bienvenidos a Launch X')
+})
+
+app.get('/explorersInNode', (req, res) => {
+    const explorer = {name: "Explorer", msg: "Hello"}
+    res.send('explorer')
+})
+
+app.get('explorers/:explorerName', (req, res) => {
+    res.send(req.params)
 })
 
 // con esto iniciamos la app
